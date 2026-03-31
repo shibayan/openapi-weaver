27a89bd3a2101b4c17913ef9644991c3650cb8df
Tatsuro Shibamura <me@shibayan.jp>
2026-03-31 14:28:15 +0900

    Remove unused Companies API call from sample application

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -2,8 +2,4 @@
 
 var client = new ApiSchemaClient(accessToken: "dummy-access-token");
 
-var list = await client.Companies.GetAsync();
-
-Console.WriteLine(list.Companies[0].GetType());
-
 Console.WriteLine(client.Tags.GetType().FullName);
